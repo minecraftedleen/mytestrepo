6b97ced0bc82b6a6cfcc4e9df52a97a6969b1730
BT_1N3_24 <sample@bths.edu>
2023-12-15 10:54:52 -0500

    changed string

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("yes very");
+        System.out.println("gene");
         System.out.println("hejefkefjkejfk");
         System.out.println("nopek");
         System.out.println("yes.lol");
