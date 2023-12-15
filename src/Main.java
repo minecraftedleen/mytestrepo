dc56c0064d42ec98cf5da057d49e6fa5f4d871f1
BT_1N3_23 <sample@bths.edu>
2023-12-15 10:51:35 -0500

    added print

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("yes very");
         System.out.println("Hello");
-        System.out.println("nopek");
+        System.out.println("not anym9re");
         System.out.println("yes.lol");
     }
 }
